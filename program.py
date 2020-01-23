f6335f9ce0e42a3ed4721791441cc1511af2e288
Vedika Parvez <vedikaparvez@hotmail.com>
2020-01-23 14:19:21 +0530

    Updated forward propagation

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 from random import seed
 from random import random
+from math import exp
 
-# Initializing a network
+## Initializing a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
     network = list()
     hidden_layer = [{'weights':[random() for i in range(n_inputs +1)]} for i in range(n_hidden)]
@@ -12,5 +13,35 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
 
 seed(1)
 network = initialize_network(2,1,2)
-for layer in network:
-    print(layer)
+
+## Forward propagation
+
+# Neuron activation
+def activate(weights, inputs):
+    activation = weights[-1] # activating the bias weight
+    for i in range(len(weights)-1): # activating the neurons minusing the bias
+        activation += weights[i] * inputs[i]
+    return activation
+
+# Neuron transfer; transfer neuron activation
+def transfer(activation):
+    return 1.0/(1.0 + exp(-activation))
+
+# Forward propagation
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+# testing forward propoagation
+network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+		[{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
+row = [1, 0, None]
+output = forward_propagate(network, row)
+print(output)
