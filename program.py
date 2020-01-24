5d0c08ea40e066a213ccec3b31d7c37ea548fa95
Vedika Parvez <vedikaparvez@hotmail.com>
2020-01-24 14:54:01 +0530

    added code for updating weights

--- program.py
+++ program.py
@@ -87,10 +87,13 @@ def backward_propagate_error(network, expected):
             neuron['delta'] = errors[j] * transfer(neuron['output'])
 
 
-# testing backpropagation of error
-network = [[{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-           [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
-expected = [0, 1]
-backward_propagate_error(network, expected)
-for layer in network:
-    print(layer)
+# update network weights with error
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = neuron['output'] for neuron in network[i - 1]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
