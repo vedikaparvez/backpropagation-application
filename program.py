bc6dfcf397d86bcd8b4fae1d75353419444c2d79
Vedika Parvez <vedikaparvez@hotmail.com>
2020-01-24 14:22:03 +0530

    added backpropagation error code

--- program.py
+++ program.py
@@ -2,32 +2,43 @@ from random import seed
 from random import random
 from math import exp
 
-## Initializing a network
+# Initializing a network
+
+
 def initialize_network(n_inputs, n_hidden, n_outputs):
     network = list()
-    hidden_layer = [{'weights':[random() for i in range(n_inputs +1)]} for i in range(n_hidden)]
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]}
+                    for i in range(n_hidden)]
     network.append(hidden_layer)
-    output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]}
+                    for i in range(n_outputs)]
     network.append(output_layer)
     return network
 
-seed(1)
-network = initialize_network(2,1,2)
 
-## Forward propagation
+seed(1)
+network = initialize_network(2, 1, 2)
+
+# Forward propagation
 
 # Neuron activation
+
+
 def activate(weights, inputs):
-    activation = weights[-1] # activating the bias weight
-    for i in range(len(weights)-1): # activating the neurons minusing the bias
+    activation = weights[-1]  # activating the bias weight
+    for i in range(len(weights)-1):  # activating the neurons minusing the bias
         activation += weights[i] * inputs[i]
     return activation
 
 # Neuron transfer; transfer neuron activation
+
+
 def transfer(activation):
     return 1.0/(1.0 + exp(-activation))
 
 # Forward propagation
+
+
 def forward_propagate(network, row):
     inputs = row
     for layer in network:
@@ -39,9 +50,47 @@ def forward_propagate(network, row):
         inputs = new_inputs
     return inputs
 
+
 # testing forward propoagation
 network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-		[{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
+           [{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
 row = [1, 0, None]
 output = forward_propagate(network, row)
-print(output)
+
+# Backpropagating error
+
+# transfer derivative to calculate the derivative of a neuron output
+
+
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+# error backpropagation and storing in neurons
+
+
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i+1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer(neuron['output'])
+
+
+# testing backpropagation of error
+network = [[{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+           [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
+expected = [0, 1]
+backward_propagate_error(network, expected)
+for layer in network:
+    print(layer)
