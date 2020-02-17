c18c6e8d0a3a45cb04fdb310b9ad6d5c33b32316
Vedika Parvez <vedikaparvez@hotmail.com>
2020-02-17 14:22:00 +0530

    train the network

--- program.py
+++ program.py
@@ -97,3 +97,20 @@ def update_weights(network, row, l_rate):
             for j in range(len(inputs)):
                 neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
             neuron['weights'][-1] += l_rate * neuron['delta']
+
+
+# Train a network for a fixed number of epochs
+
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i]-outputs[i]) **
+                              2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
