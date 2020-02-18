61c0b5f8639dc5fbe9ecdb58f578e260f25126a7
Vedika Parvez <vedikaparvez@hotmail.com>
2020-02-18 16:59:05 +0530

    convert string column to integer

--- program.py
+++ program.py
@@ -25,6 +25,19 @@ def str_column_to_float(dataset, column):
     for row in dataset:
         row[column] = float(row[column].strip())
 
+# Convert string column to integer
+
+
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
 # Initializing a network
 
 
