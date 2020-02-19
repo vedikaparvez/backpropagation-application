e5448e233c5d37a38bf0438d1f6640ad8e010b79
Vedika Parvez <vedikaparvez@hotmail.com>
2020-02-19 11:26:36 +0530

    split dataset into k folds

--- program.py
+++ program.py
@@ -54,6 +54,20 @@ def normalize_dataset(dataset, minmax):
         for i in range(len(row)-1):
             row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
 
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
 # Initializing a network
 
 
