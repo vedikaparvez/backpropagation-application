3783543d654e1da1f4fc074ac2c5cdc6be03e892
Vedika Parvez <vedikaparvez@hotmail.com>
2020-02-21 06:05:30 +0530

    code to evaluate algorithm using a cross validation split

--- program.py
+++ program.py
@@ -78,6 +78,27 @@ def accuracy_metric(actual, predicted):
             correct += 1
     return correct / float(len(actual)) * 100.0
 
+# Evaluate an algorithm using a cross validation split
+
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
 # Initializing a network
 
 
