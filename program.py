27c4165ddccd6ab9967370b214a83b96ce633db0
Vedika Parvez <vedikaparvez@hotmail.com>
2020-02-21 06:04:37 +0530

    code to calculate accuracy percentage added

--- program.py
+++ program.py
@@ -68,6 +68,16 @@ def cross_validation_split(dataset, n_folds):
         dataset_split.append(fold)
     return dataset_split
 
+# Calculate accuracy percentage
+
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
 # Initializing a network
 
 
